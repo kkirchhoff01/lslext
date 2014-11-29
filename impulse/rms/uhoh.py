d50e27b93c24ad6e6ba05600831ba18343dd250f
Quincy Wofford <qwofford@saxon.hosted.ats.ucla.edu>
2014-11-28 18:54:07 -0800

    another push to placate Kevin -QW

--- impulse/rms/uhoh.py
+++ impulse/rms/uhoh.py
@@ -18,17 +18,23 @@ b2, t2, p2 = frame2.parseID()
 b3, t3, p3 = frame3.parseID()
 b4, t4, p4 = frame4.parseID()
 
+print "THIS IS HF ON MOON DATA"
+
 print "frame 1 stuff: " + str(b1) + ":" + str(t1) + ":" + str(p1)
+print "Tuning is: " + str(frame1.getCentralFreq())
 print str(frame1.data.iq.real[:])
 print "frame 2 stuff: " + str(b2) + ":" + str(t2) + ":" + str(p2)
+print "Tuning is: " + str(frame2.getCentralFreq())
 print str(frame2.data.iq.real[:])
 print "frame 3 stuff: " + str(b3) + ":" + str(t3) + ":" + str(p3)
+print "Tuning is: " + str(frame3.getCentralFreq())
 print str(frame3.data.iq.real[:])
 print "frame 4 stuff: " + str(b4) + ":" + str(t4) + ":" + str(p4)
+print "Tuning is: " + str(frame4.getCentralFreq())
 print str(frame4.data.iq.real[:])
 
 
-print "BEGIN THE NEXT THINGY"
+print "BEGIN NEXT FRAME SET FOR HF ON"
 
 frame1 = drx.readFrame(file)
 frame2 = drx.readFrame(file)
@@ -41,10 +47,71 @@ b3, t3, p3 = frame3.parseID()
 b4, t4, p4 = frame4.parseID()
 
 print "frame 1 stuff: " + str(b1) + ":" + str(t1) + ":" + str(p1)
+print "Tuning is: " + str(frame1.getCentralFreq())
 print str(frame1.data.iq.real[:])
 print "frame 2 stuff: " + str(b2) + ":" + str(t2) + ":" + str(p2)
+print "Tuning is: " + str(frame2.getCentralFreq())
 print str(frame2.data.iq.real[:])
 print "frame 3 stuff: " + str(b3) + ":" + str(t3) + ":" + str(p3)
+print "Tuning is: " + str(frame3.getCentralFreq())
 print str(frame3.data.iq.real[:])
 print "frame 4 stuff: " + str(b4) + ":" + str(t4) + ":" + str(p4)
+print "Tuning is: " + str(frame4.getCentralFreq())
 print str(frame4.data.iq.real[:])
+
+print "CHANGE TO HF OFF"
+
+file = open('/u/data/leap/observations/056777_000085153','rb')
+
+
+frame1 = drx.readFrame(file)
+frame2 = drx.readFrame(file)
+frame3 = drx.readFrame(file)
+frame4 = drx.readFrame(file)
+
+b1, t1, p1 = frame1.parseID()
+b2, t2, p2 = frame2.parseID()
+b3, t3, p3 = frame3.parseID()
+b4, t4, p4 = frame4.parseID()
+
+print "THIS IS HF OFF MOON DATA"
+
+print "frame 1 stuff: " + str(b1) + ":" + str(t1) + ":" + str(p1)
+print "Tuning is: " + str(frame1.getCentralFreq())
+print str(frame1.data.iq.real[:])
+print "frame 2 stuff: " + str(b2) + ":" + str(t2) + ":" + str(p2)
+print "Tuning is: " + str(frame2.getCentralFreq())
+print str(frame2.data.iq.real[:])
+print "frame 3 stuff: " + str(b3) + ":" + str(t3) + ":" + str(p3)
+print "Tuning is: " + str(frame3.getCentralFreq())
+print str(frame3.data.iq.real[:])
+print "frame 4 stuff: " + str(b4) + ":" + str(t4) + ":" + str(p4)
+print "Tuning is: " + str(frame4.getCentralFreq())
+print str(frame4.data.iq.real[:])
+
+
+print "BEGIN NEXT FRAME SET FOR HF OFF"
+
+frame1 = drx.readFrame(file)
+frame2 = drx.readFrame(file)
+frame3 = drx.readFrame(file)
+frame4 = drx.readFrame(file)
+
+b1, t1, p1 = frame1.parseID()
+b2, t2, p2 = frame2.parseID()
+b3, t3, p3 = frame3.parseID()
+b4, t4, p4 = frame4.parseID()
+
+print "frame 1 stuff: " + str(b1) + ":" + str(t1) + ":" + str(p1)
+print "Tuning is: " + str(frame1.getCentralFreq())
+print str(frame1.data.iq.real[:])
+print "frame 2 stuff: " + str(b2) + ":" + str(t2) + ":" + str(p2)
+print "Tuning is: " + str(frame2.getCentralFreq())
+print str(frame2.data.iq.real[:])
+print "frame 3 stuff: " + str(b3) + ":" + str(t3) + ":" + str(p3)
+print "Tuning is: " + str(frame3.getCentralFreq())
+print str(frame3.data.iq.real[:])
+print "frame 4 stuff: " + str(b4) + ":" + str(t4) + ":" + str(p4)
+print "Tuning is: " + str(frame4.getCentralFreq())
+print str(frame4.data.iq.real[:])
+
