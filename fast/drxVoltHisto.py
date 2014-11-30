a8686b7794a02ec4636f5d750e8dafca420f0222
Quincy Wofford <qwofford@saxon.hosted.ats.ucla.edu>
2014-11-29 20:45:02 -0800

    Created class def for histo and made a test file. I'm concerned that the current iteration of drxVoltHisto is not working, because no matter the input and offset, T1P0 and T2P1 appear to have the same histo shape. -QW

--- fast/drxVoltHisto.py
+++ fast/drxVoltHisto.py
@@ -5,50 +5,89 @@ from lsl.correlator import fx as fxc
 from lsl.misc.mathutil import to_dB
 from matplotlib import pyplot as plt
 
+class drxVoltHisto:
+    def __init__(self,fileNameA, fileNameB):
+        self.histoFileA = open(fileNameA, 'rb')
+        self.histoFileB = open(fileNameB, 'rb')
 
-framesToIterate =10
-startAtFrame = 330000 
+    def compute(self, framesToIterate, startAtFrame):
+        self.framesToIterate = 4*framesToIterate
+        #Remember thre are four frames associated with a single sample in time, one for T1P0, T1P1, T2P0 and T2P1
+        self.startAtFrame = 4*startAtFrame 
+        try:
+            self.histoFileA.seek((self.startAtFrame-1)*4128,0)
+        except:
+            print "Frame start bounds may be incorrect. Must start at frame >=1"
+        self.histoArrT1P0A = []
+        self.histoArrT1P1A = []
+        self.histoArrT2P0A = []
+        self.histoArrT2P1A = []
+        try:
+            self.histoFileB.seek((self.startAtFrame-1)*4128,0)
+        except:
+            print "Frame start bounds may be incorrect. Must start at frame >=1"
+        self.histoArrT1P0B = []
+        self.histoArrT1P1B = []
+        self.histoArrT2P0B = []
+        self.histoArrT2P1B = []
+        self.beamA=-1
+        self.beamB=-1
+        self.tune1A=-1
+        self.tune1B=-1
+        self.tune2A=-1
+        self.tune2B=-1
+        i = 0
+        while i<self.framesToIterate:
+            i += 1
+            self.histoT1P0TempA = drx.readFrame(self.histoFileA)
+            self.histoArrT1P0A.extend(self.histoT1P0TempA.data.iq.real)
+            self.histoT1P1TempA = drx.readFrame(self.histoFileA)
+            self.histoArrT1P1A.extend(self.histoT1P1TempA.data.iq.real)
+            self.histoT2P0TempA = drx.readFrame(self.histoFileA)
+            self.histoArrT2P0A.extend(self.histoT2P0TempA.data.iq.real)
+            self.histoT2P1TempA = drx.readFrame(self.histoFileA)
+            self.histoArrT2P1A.extend(self.histoT2P1TempA.data.iq.real)
+            self.histoT1P0TempB = drx.readFrame(self.histoFileB)
+            self.histoArrT1P0B.extend(self.histoT1P0TempB.data.iq.real)
+            self.histoT1P1TempB = drx.readFrame(self.histoFileB)
+            self.histoArrT1P1B.extend(self.histoT1P1TempB.data.iq.real)
+            self.histoT2P0TempB = drx.readFrame(self.histoFileB)
+            self.histoArrT2P0B.extend(self.histoT2P0TempB.data.iq.real)
+            self.histoT2P1TempB = drx.readFrame(self.histoFileB)
+            self.histoArrT2P1B.extend(self.histoT2P1TempB.data.iq.real)
+            self.tune1A = (self.histoT1P0TempA.getCentralFreq()/1e6)
+            self.tune2A = (self.histoT2P0TempA.getCentralFreq()/1e6)
+            self.tune1B = (self.histoT1P0TempB.getCentralFreq()/1e6)
+            self.tune2B = (self.histoT2P0TempB.getCentralFreq()/1e6)
+            self.beamA, self.dontCare, self.irrelevant = self.histoT1P0TempA.parseID()
+            self.beamB, self.dontCare, self.irrelevant = self.histoT1P0TempB.parseID()
+        fig = plt.figure()
+        ax1 = fig.add_subplot(221)
+        fileAx1 = plt.hist(self.histoArrT1P0A[:],bins=8,histtype='step',normed=True,label = "B"+str(self.beamA)+":T"+str("%.1f" % self.tune1A)+":P0")
+        fileBx1 = plt.hist(self.histoArrT1P0B[:],bins=8,histtype='step',normed=True,label = "B"+str(self.beamB)+":T"+str("%.1f" % self.tune1B)+":P0")
+        ax1.set_xlabel('Step')
+        ax1.set_ylabel('Count')
+        ax1.legend(loc='best')
 
-hf_on = open('/u/data/leap/observations/056777_000085153', 'rb')
-hf_off = open('/u/data/leap/observations/056777_000085150', 'rb')
-lf_on = open('/u/data/leap/observations/056777_000085151', 'rb')
-lf_off = open('/u/data/leap/observations/056777_000085152', 'rb')
+        ax2 = fig.add_subplot(222)
+        fileAx2 = plt.hist(self.histoArrT1P1A[:],bins=8,histtype='step',normed=True,label = "B"+str(self.beamA)+":T"+str("%.1f" % self.tune1A)+":P1")
+        fileBx2 = plt.hist(self.histoArrT1P1B[:],bins=8,histtype='step',normed=True,label = "B"+str(self.beamB)+":T"+str("%.1f" % self.tune1A)+":P1")
+        ax2.set_xlabel('Step')
+        ax2.set_ylabel('Count')
+        ax2.legend(loc='best')
 
-hf_on.seek((startAtFrame-1)*4128,0)
-hf_off.seek((startAtFrame-1)*4128,0)
-lf_on.seek((startAtFrame-1)*4128,0)
-lf_off.seek((startAtFrame-1)*4128,0)
+        ax3 = fig.add_subplot(223)
+        fileAx3 = plt.hist(self.histoArrT2P0A[:],bins=8,histtype='step',normed=True,label = "B"+str(self.beamA)+":T"+str("%.1f" % self.tune2A)+":P0")
+        fileBx3 = plt.hist(self.histoArrT2P0B[:],bins=8,histtype='step',normed=True,label = "B"+str(self.beamB)+":T"+str("%.1f" % self.tune2A)+":P0")
+        ax3.set_xlabel('Step')
+        ax3.set_ylabel('Count')
+        ax3.legend(loc='best')
 
-hf_onArr = []
-hf_offArr = []
-lf_onArr = []
-lf_offArr = []
-i = 0
-while i<framesToIterate:
-    i += 1
-    hf_onTemp = drx.readFrame(hf_on)
-    hf_onArr.extend(hf_onTemp.data.iq.real)
-    hf_offTemp = drx.readFrame(hf_off)
-    hf_offArr.extend(hf_offTemp.data.iq.real)
-    lf_onTemp = drx.readFrame(lf_on)
-    lf_onArr.extend(lf_onTemp.data.iq.real)
-    lf_offTemp = drx.readFrame(lf_off)
-    lf_offArr.extend(lf_offTemp.data.iq.real)
+        ax4 = fig.add_subplot(224)
+        fileAx4 = plt.hist(self.histoArrT2P1A[:],bins=8,histtype='step',normed=True,label = "B"+str(self.beamA)+":T"+str("%.1f" % self.tune2A)+":P1")
+        fileBx4 = plt.hist(self.histoArrT2P1B[:],bins=8,histtype='step',normed=True,label = "B"+str(self.beamB)+":T"+str("%.1f" % self.tune2A)+":P1")
+        ax4.set_xlabel('Step')
+        ax4.set_ylabel('Count')
+        ax4.legend(loc='best')
 
-
-fig = plt.figure()
-ax1 = fig.add_subplot(121)
-hf_onx = plt.hist(hf_onArr[:],bins=8,histtype='step',normed=True,label = 'HF / On Moon')
-hf_offx = plt.hist(hf_offArr[:],bins=8,histtype='step',normed=True,label = 'HF / Off Moon')
-ax1.set_xlabel('Step')
-ax1.set_ylabel('Count')
-ax1.legend(loc='best')
-
-ax2 = fig.add_subplot(122)
-lf_onx = plt.hist(lf_onArr[:],bins=8,histtype='step',normed=True,label = 'LF / On Moon')
-lf_offx = plt.hist(lf_offArr[:],bins=8,histtype='step',normed=True,label = 'LF / Off Moon')
-ax2.set_xlabel('Step')
-ax2.set_ylabel('Count')
-ax2.legend(loc='best')
-
-plt.show()
+        plt.show()
---
+++ fast/histoTest.py
@@ -0,0 +1,4 @@
+from drxVoltHisto import drxVoltHisto
+
+comparison1 = drxVoltHisto('/u/data/leap/observations/056777_000085151','/u/data/leap/observations/056777_000085152')
+comparison1.compute(5,10)
